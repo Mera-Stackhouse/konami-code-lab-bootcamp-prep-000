c73c96a986dc987c7ebe665e2a68b206a0c1c4de
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-28 04:54:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,21 +13,26 @@ const codes = [
 
 let index = 0;
 
-document.body.addEventListener('keydown', init);
-
-function init(e) {
-  // your code here
-  const key = e.key;
- 
-  if (key === codes[index]) {
-    index++;
-    if (index === codes.length) {
-      alert("Hurray!");
+function init() {
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    console.log(key);
+    if (key === codes[index]) {
+      index++;
+   
+      if (index === codes.length) {
+        alert("Hurray!");
+   
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
-  }
-  
+  });
 }
+
+
+
+//init();
+
 
